7b1a8977cb4f0f4738ef0cec9ed485307b1c880c
kuzyukov <egor.kyzukov@gmail.com>
2018-03-26 00:18:04 +0500

    dark sidebar

--- aggregation/urls.py
+++ aggregation/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.agregationPage, name = "agregationPage")
+    path('aggregation/', views.agregationPage, name = "agregationPage"),
+    path('about/', views.aboutPage, name = "aboutPage"),
+    path('main/', views.agregationPage, name = "mainPage"),
+    path('statistic/', views.statisticPage, name = "statisticPage"),
 ]
--- aggregation/views.py
+++ aggregation/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def agregationPage(request):
     return render(request, 'aggregation/aggregationPage.html')
+
+def aboutPage(request):
+    return render(request, 'aggregation/aboutPage.html')
+
+def mainPage(request):
+    return render(request, 'aggregation/mainPage.html')
+
+def statisticPage(request):
+    return render(request, 'aggregation/statisticPage.html')
