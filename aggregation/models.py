82428f24cc9b0135d69ea9303b7911c8430d3166
Kuzyukov <egor.kyzukov@gmail.com>
2018-04-03 00:39:25 +0500

    about page edited

--- aggregation/models.py
+++ aggregation/models.py
@@ -1,91 +1,70 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-
-
-
-
 """
-
-
-class AutoRuObject(models.Model):
-    AdName = models.CharField("Ссылка на объявление", max_lenght=500)
-    linkOnAd = models.CharField("Ссылка на объявление", max_lenght=500)
-    ModelAuto = models.ForeignKey(ModelOfAuto, verbose_name="Модель автомобиля")
-    MarkAuto = models.ForeignKey(MarkOfAuto, verbose_name="Марка автомобиля")
-    BodyType = models.ForeignKey(BodyTypeOfAuto, verbose_name="Тип кухова автомобиля")
-    Color = models.ForeignKey(ColorOfAuto, verbose_name="Цвет автомобиля")
-    Mileage  = models.IntegerField()
-    YearOfIssue  = models.IntegerField()
-    EngineType = models.BooleanField()
-    TransmissionType = models.ForeignKey(TransmissionTypeOfAuto, verbose_name="Тип коробки передач")
-    DriveUnit = models.ForeignKey(DriveUnitOfAuto, verbose_name="Привод")
-    EngineCapacity = models.IntegerField()
-    EnginePower = models.IntegerField()
-    DateOfPublic = models.DateTimeField()
-    NumberOfOwners = models.IntegerField()
-    Description = models.TextField()
-    AvailabilityOfDocuments = models.DateTimeField()
-    Condition = models.BooleanField()
-    Swap = models.BooleanField()
-    DateParcing = models.DateTimeField("Дата, когда объявление было загружено(парсером) в базу", auto_now_add=True)
-
-
 class AvitoObject(models.Model):
-    AdName = models.CharField("Ссылка на объявление", max_lenght=500)
-    linkOnAd = models.CharField("Ссылка на объявление", max_lenght=500)
+    AdName = models.CharField("Ссылка на объявление", max_lenght=100)
+    linkOnAd = models.CharField("Ссылка на объявление", max_lenght=1500)
     ModelAuto = models.ForeignKey(ModelOfAuto, verbose_name="Модель автомобиля")
     MarkAuto = models.ForeignKey(MarkOfAuto, verbose_name="Марка автомобиля")
+    Price = models.IntegerField()
+    YearOfIssue  = models.IntegerField()
+    Mileage  = models.IntegerField()
     BodyType = models.ForeignKey(BodyTypeOfAuto, verbose_name="Тип кухова автомобиля")
     Color = models.ForeignKey(ColorOfAuto, verbose_name="Цвет автомобиля")
-    Mileage  = models.IntegerField()
-    YearOfIssue  = models.IntegerField()
-    EngineType = models.BooleanField()
+    EngineCapacity = models.IntegerField()
+    kolvoDverey = models.IntegerField()
     TransmissionType = models.ForeignKey(TransmissionTypeOfAuto, verbose_name="Тип коробки передач")
     DriveUnit = models.ForeignKey(DriveUnitOfAuto, verbose_name="Привод")
-    EngineCapacity = models.IntegerField()
-    EnginePower = models.IntegerField()
-    DateOfPublic = models.DateTimeField()
     NumberOfOwners = models.IntegerField()
+    EnginePower = models.IntegerField()
     Description = models.TextField()
-    AvailabilityOfDocuments = models.DateTimeField()
-    Condition = models.BooleanField()
-    Swap = models.BooleanField()
-    DateParcing = models.DateTimeField("Дата, когда объявление было загружено(парсером) в базу", auto_now_add=True)
+    Picture = models.CharField("Ссылка на объявление", max_lenght=1500)
 
 class DromObject(models.Model):
-    AdName = models.CharField("Ссылка на объявление", max_lenght=500)
-    linkOnAd = models.CharField("Ссылка на объявление", max_lenght=500)
+    AdName = models.CharField("Ссылка на объявление", max_lenght=100)
+    linkOnAd = models.CharField("Ссылка на объявление", max_lenght=1500)
     ModelAuto = models.ForeignKey(ModelOfAuto, verbose_name="Модель автомобиля")
     MarkAuto = models.ForeignKey(MarkOfAuto, verbose_name="Марка автомобиля")
-    BodyType = models.ForeignKey(BodyTypeOfAuto, verbose_name="Тип кухова автомобиля")
+    YearOfIssue  = models.IntegerField()
     Color = models.ForeignKey(ColorOfAuto, verbose_name="Цвет автомобиля")
     Mileage  = models.IntegerField()
+    Price = models.IntegerField()
+
+
+class AMruObject(models.Model):
+    AdName = models.CharField("Ссылка на объявление", max_lenght=100)
+    linkOnAd = models.CharField("Ссылка на объявление", max_lenght=1500)
+    ModelAuto = models.ForeignKey(ModelOfAuto, verbose_name="Модель автомобиля")
+    MarkAuto = models.ForeignKey(MarkOfAuto, verbose_name="Марка автомобиля")
     YearOfIssue  = models.IntegerField()
-    EngineType = models.BooleanField()
-    TransmissionType = models.ForeignKey(TransmissionTypeOfAuto, verbose_name="Тип коробки передач")
-    DriveUnit = models.ForeignKey(DriveUnitOfAuto, verbose_name="Привод")
-    EngineCapacity = models.IntegerField()
-    EnginePower = models.IntegerField()
-    DateOfPublic = models.DateTimeField()
-    NumberOfOwners = models.IntegerField()
-    Description = models.TextField()
-    AvailabilityOfDocuments = models.DateTimeField()
-    Condition = models.BooleanField()
-    Swap = models.BooleanField()
-    DateParcing = models.DateTimeField("Дата, когда объявление было загружено(парсером) в базу", auto_now_add=True)
+    Color = models.ForeignKey(ColorOfAuto, verbose_name="Цвет автомобиля")
+    Mileage  = models.IntegerField()
+    Price = models.IntegerField()
+
+class YoulaObject(models.Model):
+    AdName = models.CharField("Ссылка на объявление", max_lenght=100)
+    linkOnAd = models.CharField("Ссылка на объявление", max_lenght=1500)
+    ModelAuto = models.ForeignKey(ModelOfAuto, verbose_name="Модель автомобиля")
+    MarkAuto = models.ForeignKey(MarkOfAuto, verbose_name="Марка автомобиля")
+    YearOfIssue  = models.IntegerField()
+    Color = models.ForeignKey(ColorOfAuto, verbose_name="Цвет автомобиля")
+    Mileage  = models.IntegerField()
+    Price = models.IntegerField()
+
 
 
 class MainAdObject(models.Model):
     user = models.ForeignKey(User, verbose_name="Автор комментария")
-    AutoRu = models.ForeignKey(AutoRuObject, verbose_name="АвтоРУ")
     Avito = models.ForeignKey(AvitoObject, verbose_name="Авито")
     Drom = models.ForeignKey(DromObject, verbose_name="Дром")
+    AMru = models.ForeignKey(AMruObject, verbose_name="Дром")
+    Youla = models.ForeignKey(YoulaObject, verbose_name="Дром")
 
 
 
 
-    
+
 class Comments(models.Model):
     user = models.ForeignKey(
         User,
